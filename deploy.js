cc5f3304c87bb0005d0fa4253f62ff711f01d763
zibundayo <156084552+zibundayo@users.noreply.github.com>
2024-05-17 10:52:23 +0900

    Add files via upload

---
+++ deploy.js
@@ -0,0 +1,47 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v10');
+const fs = require('fs');
+const { CLIENT_ID,TOKEN } = process.env;
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    // 他の必要な意図を追加する
+  ],
+});
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  if (command.data && typeof command.data.toJSON === 'function') {
+    commands.push(command.data.toJSON());
+  } else {
+    console.error(`Error: Command data is invalid for command "${command.name}".`);
+  }
+}
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing global application (/) commands.');
+
+    await rest.put(
+      Routes.applicationCommands(CLIENT_ID),
+      { body: commands },
+    );
+
+    console.log('Successfully reloaded global application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+
+
+
+client.login(TOKEN);
